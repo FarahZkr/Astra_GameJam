d79554831266b621c73dcbca5c3392bc7da875a9
NAD - GameJam <gamejam@LAB>
2023-09-15 20:40:26 -0400

    Added packages

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveHuman : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     public float speed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))* speed) ;
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //transform.Translate(new)
+        }
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed);
     }
 }

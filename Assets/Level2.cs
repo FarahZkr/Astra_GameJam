162e69aacc777c5e1d511c2f57716cfc13b4f8b8
NAD - GameJam <gamejam@LAB>
2023-09-16 13:32:25 -0400

    Added new animations and functionalities

--- Assets/Level2.cs
+++ Assets/Level2.cs
@@ -5,27 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class Level2 : MonoBehaviour
 {
+    public float playersEntered = 0;
+    public float delay = 5;
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Human")
+            Debug.Log(playersEntered);
+        if (collision.gameObject.name == "Human" || collision.gameObject.name == "Cat")
         {
-            Debug.Log("cool");
-            CompleteLevel();
+            playersEntered++;
+            Destroy(collision.gameObject);
+            if (playersEntered == 1)
+            {
+                Invoke("CompleteLevel", 2);
+            }
+
+            else if (playersEntered == 2)
+            {
+                CompleteLevel();
+            }
         }
     }
     private void CompleteLevel()
     {
-        SceneManager.LoadScene("Level 2");
+        //SceneManager.LoadScene("Level 2");
+    }
+    IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(7f);
+        // DO STUFF AFTER 3 SECONDS
     }
 }

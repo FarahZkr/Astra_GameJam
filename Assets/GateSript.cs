a08bdb9480dc0a90f686068279ef8ee2b383bba1
NAD - GameJam <gamejam@LAB>
2023-09-16 14:43:54 -0400

    gate behaviour done

--- Assets/GateSript.cs
+++ Assets/GateSript.cs
@@ -8,16 +8,21 @@ public class GateSript : MonoBehaviour
 
     Animator animator;
     public GateButton button;
+    public GateButton button1;
+    BoxCollider2D boxCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.SetBool(isDoorOpenParamName, button.isGateOpen);
+        bool isOpen = button.isGateOpen || button1.isGateOpen;
+        animator.SetBool(isDoorOpenParamName, isOpen);
+        boxCollider.enabled = !isOpen;
     }
 }

b7a94a33f40bc9dcb8245bd9b23347cd3fba6ca4
Farah Zkr <79024631+FarahZkr@users.noreply.github.com>
2023-09-17 12:35:03 -0400

    Changes

--- Assets/Scripts/AltCatMovement.cs
+++ Assets/Scripts/AltCatMovement.cs
@@ -12,10 +12,13 @@ public class AltCatMovement : MonoBehaviour
     NavMeshAgent _agent;
     Animator _animator;
     SpriteRenderer _spriteRenderer;
+    bool isRight;
+    bool canJump;
 
     void Start()
     {
-
+        isRight = false;
+        canJump = true;
         m_Rigidbody = GetComponent<Rigidbody2D>();
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponentInChildren<Animator>();
@@ -25,63 +28,92 @@ public class AltCatMovement : MonoBehaviour
     void Update()
     {
         //Apply a force to the RigidBody to make the player move
-        if (Input.GetButton("Horizontal2"))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded && canJump)
         {
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (isRight)
             {
-                _spriteRenderer.flipX = false;
-                _animator.SetInteger("side", 2);
+                _spriteRenderer.flipX = true;
+                _animator.SetInteger("jump", 1);
             }
             else
             {
-                _animator.SetInteger("side", 1);
+                _spriteRenderer.flipX = false;
+                _animator.SetInteger("jump", 2);
+            }
+
+            Vector2 up = new Vector2(0, jumpForce);
+            m_Rigidbody.AddForce(up, ForceMode2D.Impulse);
+        }
+        else if (Input.GetButton("Horizontal2"))
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                isRight = false;
+                _spriteRenderer.flipX = false;
+                if (isGrounded) _animator.SetInteger("side", 2);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                isRight = true;
+                if (isGrounded) _animator.SetInteger("side", 1);
                 _spriteRenderer.flipX = true;
             }
             m_Rigidbody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal2"), m_Rigidbody.velocity.y);
         }
-        else
+        else if(!(Input.GetButton("Horizontal2")) || !(Input.GetKeyDown(KeyCode.UpArrow)))
         {
             _animator.SetInteger("side", 0);
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
-        {
-            Vector2 up = new Vector2(0, jumpForce);
-            m_Rigidbody.AddForce(up, ForceMode2D.Impulse);
         }
         
 
         // Slow Down the player
-
         m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x * 0.5f, m_Rigidbody.velocity.y);
-
-
-
     }
-    void OnCollisionEnter2D(Collision2D col)
+
+    void OnTriggerEnter2D(Collider2D col)
     {
-        Vector2 v = transform.position;
+         if(col.gameObject.layer == 7)
+        {
+            _animator.SetInteger("jump", 0);
+            canJump = false;
+        }
+
         if (col.gameObject.layer == 3)
         {
             isGrounded = true;
-            Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+            _animator.SetInteger("jump", 0);
         }
-        if (col.gameObject.layer == 6)
+       
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 7)
         {
-
+            canJump = true;
         }
-        if (col.gameObject.tag == "Mushroom")
+        if (collision.gameObject.layer == 3)
+        {
+            isGrounded = false;
+            if (isRight)
+            {
+                _spriteRenderer.flipX = true;
+                _animator.SetInteger("jump", 1);
+            }
+            else
+            {
+                _spriteRenderer.flipX = false;
+                _animator.SetInteger("jump", 2);
+            }
+        }
+  
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Mushroom")
         {
             Vector2 up = new Vector2(0, jumpForce * 2);
             m_Rigidbody.AddForce(up, ForceMode2D.Impulse);
         }
     }
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 3)
-        {
-            isGrounded = false;
-            Debug.Log("WHATS GOING ON");
-        }
-
-    }
+    
 }

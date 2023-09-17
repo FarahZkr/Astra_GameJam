d415d6c6b6d1d2a22f14d5151c9d9aa224a9fdd3
Farah Zkr <79024631+FarahZkr@users.noreply.github.com>
2023-09-17 00:00:19 -0400

    Player Jump

--- Assets/Scripts/AltMovement.cs
+++ Assets/Scripts/AltMovement.cs
@@ -13,6 +13,7 @@ public class AltMovement : MonoBehaviour
     Animator _animator;
     SpriteRenderer _spriteRenderer;
     bool isRight;
+    bool jumped = false;
 
     void Start()
     {
@@ -25,48 +26,43 @@ public class AltMovement : MonoBehaviour
 
     void Update()
     {
-        if (isRight)
-            Debug.Log("Right");
-        else
-            Debug.Log("Left");
+     
         //Apply a force to the RigidBody to make the player move
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            // _animator.SetInteger("side", 0);
             if (isRight)
             {
                 _spriteRenderer.flipX = false;
-                _animator.SetInteger("side", 3);
-                Debug.Log("rihgt");
+                _animator.SetInteger("jump", 1);
             }
             else
             {
-            //_spriteRenderer.flipX = true;
-            _animator.SetInteger("side", 3);
+                _spriteRenderer.flipX = true;
+                //_animator.SetInteger("side", 0);
+                _animator.SetInteger("jump", 2);
             }
             Vector2 up = new Vector2(0, jumpForce);
             m_Rigidbody.AddForce(up, ForceMode2D.Impulse);
+            jumped = true;
         }
-        else
-        {
-            _animator.SetInteger("side", 0);
-        }
-        if (Input.GetButton("Horizontal"))
+        else if (Input.GetButton("Horizontal"))
         {
             if (Input.GetKey(KeyCode.A))
             {
                 isRight = false;
                 _spriteRenderer.flipX = true;
-                _animator.SetInteger("side", 2);
+                if (isGrounded) _animator.SetInteger("side", 2);
             }
-            else
+            else if (Input.GetKey(KeyCode.D))
             {
                 isRight = true;
-                _animator.SetInteger("side",1);
+                if (isGrounded) _animator.SetInteger("side", 1);
                 _spriteRenderer.flipX = false;
             }
             m_Rigidbody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), m_Rigidbody.velocity.y);
         }
-        else
+        else if (!(Input.GetButton("Horizontal")) || !(Input.GetKeyDown(KeyCode.Space)))
         {
             _animator.SetInteger("side", 0);
         }
@@ -76,12 +72,29 @@ public class AltMovement : MonoBehaviour
 
         m_Rigidbody.velocity = new Vector2(m_Rigidbody.velocity.x * 0.5f, m_Rigidbody.velocity.y);
 
+        //if (jumped)
+        //{
+        //    if (isRight)
+        //    {
+        //        _spriteRenderer.flipX = false;
+        //        _animator.SetInteger("jump", 1);
+        //    }
+        //    else
+        //    {
+        //        _spriteRenderer.flipX = true;
+        //        //_animator.SetInteger("side", 0);
+        //        _animator.SetInteger("jump", 2);
+        //    }
+        //}
+
     }
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.layer == 3)
         {
             isGrounded = true;
+            _animator.SetInteger("jump", 0);
+            jumped = false;
             //Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
         }
 
@@ -99,6 +112,17 @@ public class AltMovement : MonoBehaviour
         if (collision.gameObject.layer == 3)
         {
             isGrounded = false;
+            if (isRight)
+            {
+                _spriteRenderer.flipX = false;
+                _animator.SetInteger("jump", 1);
+            }
+            else
+            {
+                _spriteRenderer.flipX = true;
+                //_animator.SetInteger("side", 0);
+                _animator.SetInteger("jump", 2);
+            }
             Debug.Log("WHATS GOING ON");
         }
 

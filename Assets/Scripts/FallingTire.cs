12848ae91fb2e4c11d9523c70c86a43e735884b9
Lorenzo <61562227+Lorenzo767@users.noreply.github.com>
2023-09-17 09:49:45 -0400

    Projectiles for level 3

---
+++ Assets/Scripts/FallingTire.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class FallingTire : MonoBehaviour
+{
+    [SerializeField] GameObject tirePrefab;
+
+    private float minX = -10, maxX = 10;
+    private float timeMin = 2, timeMax = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnTires", Random.Range(timeMin, timeMax));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnTires()
+    { 
+        float randPosX = Random.Range(minX, maxX);
+        tirePrefab = Instantiate(tirePrefab, new Vector2(randPosX, 16.5f), Quaternion.identity);
+        Debug.Log("tire location: " + randPosX);
+
+        
+        Invoke("SpawnTires", Random.Range(timeMin, timeMax));
+    
+    }
+}
